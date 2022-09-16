692899bc2a45a17ff63e498666ca7bf24be98838
EngineerSahil <77582744+EngineerSahil@users.noreply.github.com>
2022-09-16 09:28:34 +0600

    structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useState } from 'react';
+import {AiOutlinePlus} from "react-icons/ai"
+import Todo from './Todo';
+
+const style ={
+  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`
+}
+
 
 function App() {
+
+  const [todos,setTodos] = useState(["learn react", "learn coding"])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={style.bg}>
+      <div className={style.container}>
+        <h3 className={style.heading}>Todo App</h3>
+          <form className={style.form}>
+            <input className={style.input} type="text" placeholder='Add Todos' />
+            <button className={style.button}><AiOutlinePlus size={30}/></button>
+          </form>
+          
+          <ul>
+            {
+              todos.map((todo, index) =>(
+                <Todo key={index} todo={todo} />
+              ))
+            }
+          </ul>
+        
+      </div>
+
     </div>
   );
 }
